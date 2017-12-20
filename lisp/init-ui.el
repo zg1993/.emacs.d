41a3566a9e50b33eeab3faca04c5ef59664357f8
zhou.gang <zhou.gang@mgm-iot.com>
2017-12-20 19:28:21 +0800

    list

---
+++ lisp/init-ui.el
@@ -0,0 +1,10 @@
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(setq inhibit-splash-screen t)
+(global-linum-mode t)
+(global-hl-line-mode t)
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+(provide 'init-ui)
