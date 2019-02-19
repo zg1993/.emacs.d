b81879632e8b13fe64d4e8278d6afc2eaa2efd14
zg1993 <13122780440@163.com>
2019-02-19 21:43:01 +0800

    second

---
+++ init.el
@@ -0,0 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(global-linum-mode t)
