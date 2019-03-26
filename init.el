3c9e1ca413d69f191dde2687defc78607066fc45
zhou.gang <zhou.gang@mgm-iot.com>
2019-03-26 03:16:14 +0000

    my-package manage

--- init.el
+++ init.el
@@ -1,3 +1,33 @@
+;; package install
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  )
+(package-initialize)
+(defvar my-packages '(
+		     highlight-parentheses
+		     ))
+;; judge have package need install
+
+(defun my-packages-installed-p()
+  (loop for pkg in my-packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t))
+  )
+;; package install
+(require 'cl)
+(unless (my-packages-installed-p)
+  (package-refresh-contents)
+  (message "%s" "refreshing")
+  (dolist (pkg my-packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg)))
+  )
+(global-highlight-parentheses-mode t)
+
+
+
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (global-linum-mode t)
